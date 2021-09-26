c457ddeb00e3b467cf94e5ab1f729713aa844e6a
ism90 <ianmason5@gmail.com>
2021-09-26 18:23:06 +0100

    dynamic class names for slots

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-class Game {
-  constructor() {}
+const getSlotsHTML = document.querySelectorAll(".grid__circle");
+console.log(getSlotsHTML);
 
-  flipCoin() {}
+class Game {
+  constructor(slot) {
+    this.slot = slot;
+  }
+
+  getSlotArray() {
+    const slotArray = Array.from(getSlotsHTML);
+
+    for (let i = 0; i < slotArray.length; i++) {
+      slotArray[i].classList.add("slot" + i);
+    }
+  }
 }
+
+const game = new Game(getSlotsHTML);
+
+game.getSlotArray();
