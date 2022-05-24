801dd0564f9c6d2775361966f228ea3c59a79fdf
ism90 <ianmason5@gmail.com>
2022-05-24 14:58:41 +0100

    made Class global

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,69 @@
+
+
+
+let nextColor = "red";
+const columnsArray = [];
+const slotsArray = [];
+
+// Class for Creating Slots and Handling Slot Clicks
+class Slot {
+  constructor(elementHTML, column, row) {
+    this.column = column;
+    this.row = row;
+    this.elementHTML = elementHTML;
+    //
+    this.playState = "";
+  }
+
+  clicked() {
+    const el = this.elementHTML;
+    // Checks if clickable
+    if (!el.classList.contains("clickable")) return;
+    el.style.backgroundColor = nextColor;
+    this.playState = nextColor;
+    // remove clickable class after click
+    el.classList.replace("clickable", "clicked");
+    // make next element clickable working upwards for bottom
+    if (slotsArray[this.column][this.row - 1]) {
+      slotsArray[this.column][this.row - 1].elementHTML.classList.add(
+        "clickable",
+        nextColor
+      );
+    }
+
+    // Check for Game Over and handle
+    if (isDraw(slotsArray)) {
+      gameOver(nextColor);
+    }
+
+    if (isWinner(this.column, this.row, nextColor, slotsArray)) {
+      gameOver(nextColor);
+    }
+
+    // Change player color
+    let oldColor = nextColor;
+
+    if (nextColor == "red") {
+      nextColor = "blue";
+    } else {
+      nextColor = "red";
+    }
+
+    document.querySelectorAll(".clickable").forEach((el) => {
+      el.classList.remove(oldColor);
+      el.classList.add(nextColor);
+    });
+  }
+}
+
 // Game Builder
 const newGame = (button) => {
   button.style.display = "none";
   const game = document.querySelector(".game-wrapper");
-  const columnsArray = [];
-  const slotsArray = [];
+
   // console.log(slotsArray);
 
   // Game 'state' for each slot (will be empty, blue or red)
-  let nextColor = "red";
 
   // Create Column Arrays
   for (let i = 0; i < 7; i++) {
@@ -15,57 +71,6 @@ const newGame = (button) => {
     column.className = "grid column";
     game.appendChild(column);
     columnsArray.push(column);
-  }
-
-  // Class for Creating Slots and Handling Slot Clicks
-  class Slot {
-    constructor(elementHTML, column, row) {
-      this.column = column;
-      this.row = row;
-      this.elementHTML = elementHTML;
-      //
-      this.playState = "";
-    }
-
-    clicked() {
-      const el = this.elementHTML;
-      // Checks if clickable
-      if (!el.classList.contains("clickable")) return;
-      el.style.backgroundColor = nextColor;
-      this.playState = nextColor;
-      // remove clickable class after click
-      el.classList.replace("clickable", "clicked");
-      // make next element clickable working upwards for bottom
-      if (slotsArray[this.column][this.row - 1]) {
-        slotsArray[this.column][this.row - 1].elementHTML.classList.add(
-          "clickable",
-          nextColor
-        );
-      }
-
-      // Check for Game Over and handle
-      if (isDraw(slotsArray)) {
-        gameOver(nextColor);
-      }
-
-      if (isWinner(this.column, this.row, nextColor, slotsArray)) {
-        gameOver(nextColor);
-      }
-
-      // Change player color
-      let oldColor = nextColor;
-
-      if (nextColor == "red") {
-        nextColor = "blue";
-      } else {
-        nextColor = "red";
-      }
-
-      document.querySelectorAll(".clickable").forEach((el) => {
-        el.classList.remove(oldColor);
-        el.classList.add(nextColor);
-      });
-    }
   }
 
   // Create Slots & Push to Column Array
